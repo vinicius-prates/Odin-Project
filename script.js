47f1c1345ea99081d4a62ee57ba1c80f85c7bf4d
Vinicius  Prates <89698942+vinicius-prates@users.noreply.github.com>
2022-07-01 13:10:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
     // Usamos caps lock para declarar variaveis que geralmente são dificeis de lembrar
 
     //Sempre devemos nomear as variaveis de maneira correta, deixando claro pra que ela serve e 
-    // de maneira LÚCIDA. Assim mantemos o a regra do clean code
+    // de maneira LÚCIDA. Assim mantemos a regra do clean code
 
 
 // Mostra um alerta no topo da tela
@@ -36,3 +36,52 @@
 
     document.getElementById("demo").innerHTML = number_sum;
 
+    //Operators
+
+    let x = 1, y = 3
+    
+    // O termo abaixo é considerado um 'unary operator'
+    x = -x
+    
+
+    console.log(x) // -1, unary negation was applied
+    console.log(y - x) // 2, binary operator minus was applied
+
+    /*
+      Addition +
+      Subtraction -
+      Multiplication *
+      Division /
+      Remainder %
+      Exponentiation ** 
+    */
+    
+    
+      console.log('1' + 2) // "12"
+    
+    //quando concatenamos uma string com um número, ambos viram apenas uma string
+    //porém quando somamos um número com uma string, elas viram apenas um número
+    //podemos perceber que a regra muda de acordo com a ordem
+    
+    console.log( 6 - '2' ); // 4, converts '2' to a number
+    console.log( '6' / '2' ); // 3, converts both operands to numbers
+
+    // No effect on numbers
+    console.log( +x ); // 1
+
+    let z = -2;
+    console.log( +z ); // -2
+
+    // Converts non-numbers
+    console.log( +true ); // 1
+    console.log( +"" );   // 0
+
+    let apples = "2";
+    let oranges = "3";
+
+    console.log( apples + oranges ); // "23", the binary plus concatenates strings
+    // both values converted to numbers before the binary plus
+    console.log( +apples + +oranges ); // 5
+
+    // no caso de cima, os sianis de + antes das variaveis, e transformam em números 
+    // alert( Number(apples) + Number(oranges) ); // 5
